492a6cf28c7bb789dbfb1cc496f3c84f809ea332
Amritanshu Agrawal <agrawalamritanshu82@gmail.com>
2020-07-28 00:56:28 +0530

    Add files via upload

---
+++ template.py
@@ -0,0 +1,19 @@
+from sys import stdin,stdout
+import math
+from bisect import bisect_left, bisect_right
+input=stdin.readline
+def print(x='',y='',end='\n'):
+    if y=='':
+        stdout.write(str(x)+end)
+    else:
+        stdout.write(str(x)+' '+str(y)+end)
+
+
+def solve():    
+
+
+    
+tt=1
+tt=int(input())
+for __ in range(tt):
+    solve()
