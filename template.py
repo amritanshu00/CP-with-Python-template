cffe9f8e9ba36406d4b1b243f3265aad5469c7e7
Amritanshu Agrawal <agrawalamritanshu82@gmail.com>
2020-07-31 16:17:30 +0530

    Update template.py

--- template.py
+++ template.py
@@ -2,11 +2,13 @@ from sys import stdin,stdout
 import math
 from bisect import bisect_left, bisect_right
 input=stdin.readline
-def print(x='',y='',end='\n'):
-    if y=='':
-        stdout.write(str(x)+end)
-    else:
-        stdout.write(str(x)+' '+str(y)+end)
+def print(*args,end='\n'):
+    s=[]
+    for i in args:
+        s.append(str(i)+' ')
+    s=''.join(s)
+    stdout.write(s+end)
+    
 
 
 def solve():    
